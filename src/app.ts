7b4b647d539e596ef9e0eb977589db888676f93f
Jyrki Kokkola <j.j.kokkola@gmail.com>
2025-07-06 23:55:19 +0300

    rest test

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,15 @@ app.get('/', (_req: Request, res: Response) => {
   res.send('Hello, world!')
 })
 
+app.get('/rest-test', (_req: Request, res: Response) => {
+  res
+    .json({
+      message: 'This is a REST test endpoint',
+      timestamp: new Date().toISOString()
+    })
+    .status(200)
+})
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
 })
