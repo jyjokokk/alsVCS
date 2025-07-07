b179fe61cedbcbe21f890b140f303115408a475d
Jyrki Kokkola <j.j.kokkola@gmail.com>
2025-07-07 04:50:25 +0300

    o

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@ app.get('/rest-test', (_req: Request, res: Response) => {
     })
     .status(200)
 })
+// t
 
 app.post('/rest-test', (req: Request, res: Response) => {
   const requestBody = req.body
